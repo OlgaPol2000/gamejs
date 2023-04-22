c79d4b46e545bf00e4c9b2792413477869866b8c
OlgaPol2000 <olgapolischukinstagram@gmail.com>
2023-04-22 15:57:07 +0300

    commit

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const timeEl = document.querySelector("#time");
 const board = document.querySelector("#board");
 const reload = document.querySelector(".game__new");
 let time = 0;
+let score = 0;
 
 startBtn.addEventListener("click", (event) => {
   event.preventDefault();
@@ -24,10 +25,18 @@ timeList.addEventListener("click", (event) => {
     screens[1].classList.add("up");
     startGame();
   }
-  console.log(time);
+});
+
+board.addEventListener("click", (event) => {
+  if (event.target.classList.contains("circle")) {
+    score++;
+    event.target.remove();
+    createRandomCircle();
+  }
 });
 function startGame() {
   setInterval(decreaseTime, 1000);
+  createRandomCircle();
   setTime(time);
 }
 
@@ -47,7 +56,33 @@ function setTime(value) {
   timeEl.innerHTML = `00:${value}`;
 }
 
-function finishGame() {}
+function finishGame() {
+  timeEl.parentNode.classList.add("hide");
+  board.innerHTML = `<h2 class='board__inner'> Ваш рахунок: ${score} </h2>`;
+}
+
+function createRandomCircle() {
+  const circle = document.createElement("div");
+  const size = getRandomNumber(20, 60);
+  const { width, height } = board.getBoundingClientRect();
+  const x = getRandomNumber(0, width - size);
+  const y = getRandomNumber(0, height - size);
+
+  circle.classList.add("circle");
+  circle.style.width = `${size}px`;
+  circle.style.height = `${size}px`;
+  circle.style.top = `${y}px`;
+  circle.style.left = `${x}px`;
+  board.append(circle);
+}
+
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+reload.addEventListener("click", () => {
+  location.reload();
+});
+
 reload.addEventListener("click", () => {
   location.reload();
 });
